2137fb8fb00bba3b786a09ee6f3b5041d6823db7
leonrworker <leonrworker@gmail.com>
2021-11-08 10:37:27 -0300

    Update

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <title>To-Do List</title>
     <link rel="stylesheet" href="style.css">
 </head>
@@ -12,10 +13,15 @@
         <h1>To-Do List</h1>
         <div class="sub-section">
             <form action="">
-                <!-- <input type="text" name="title" placeholder="Adicionar tarefa">
-                <button type="submit" name="insert">CADASTRAR</button> -->
+                <input type="text" name="title" placeholder="Adicionar tarefa">
+                <button type="submit" name="insert">CADASTRAR</button>
             </form>
         </div>
+        <ul class="columns">
+            <li class="task tsk">Tarefa</li>
+            <li class="date dat">Data</li>
+            <li class="options opt">Opções</li>
+        </ul>
         <?php 
             // Inclui a conexão
             require('./controllers/connection.php');
@@ -26,17 +32,20 @@
             while($data = mysqli_fetch_array($search)):
         ?>
         <div class="to-do">
-            <input class ="checked" type="checkbox">
-            <h2 class ="title">
+            <span class ="title">
                 <?php
                     echo $data['title'];
                 ?>
-            </h2>
-            <small class ="date">Prazo:  
+            </span>
+            <small class ="date">  
                 <?php
                     echo $data['date'];
                 ?>
             </small>
+            <div class="options buttons">
+                <input class ="checked" type="checkbox">
+                <span class="delete"><a href ="./controllers/deletar.php?id=<?php echo $data['id'];?>" onclick="return confirm('Você dezeja mesmo deletar a tarefa :<?php echo $data['date'];?>');"><i class="fas fa-trash-alt"></i></a></span>
+            </div>
         </div>
         <?php
             endwhile;
