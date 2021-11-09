3de2ca2b8d1783d749928c19c195fdd694a0d19f
leonrworker <leonrworker@gmail.com>
2021-11-09 16:28:54 -0300

    Update

--- controllers/insert.php
+++ controllers/insert.php
@@ -8,10 +8,10 @@
         // Nome da atividade
         $title = $_POST['title'];
         // Realizando query
-        mysqli_query($conn, "INSERT INTO task(title, date) VALUES('$title', '$date')");
+        mysqli_query($conn, "INSERT INTO task(title, date, checked) VALUES('$title', '$date', false)");
         // Finalizar query
         mysqli_close($conn);
         // Retornar para a listagem
         header('location:../index.php');
     }
-?>
+?>,
