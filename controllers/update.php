2c90264d0d7b4c59ed1df38de20fa379d790ae2c
Ana Gabriela <anagabrieladev@gmail.com>
2021-11-16 18:32:11 -0300

    fix: update the database if the task has completed

--- controllers/update.php
+++ controllers/update.php
@@ -4,7 +4,11 @@
     // ID do registro
     $id = $_GET['id'];
     // Realizando query
-    mysqli_query($conn,"UPDATE `task` SET `checked` = '1' WHERE `task`.`id` = $id");
+    $query = mysqli_query($conn,"SELECT * FROM `task`  WHERE `task`.`id` = $id", MYSQLI_USE_RESULT);
+    $result = $query->fetch_object();
+    $status = $result -> checked == '1' ? '0' : '1';
+    mysqli_free_result($query);
+    mysqli_query($conn,"UPDATE `task` SET `checked` = $status WHERE `task`.`id` = $id");
     // Finalizando query
     mysqli_close($conn);
     // Retornar para a listagem
